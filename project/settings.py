8d9dd3d8ace01e8d2eec0e299e29ec097c1c5b1c
bodegram <samuelawofisan1@gmail.com>
2023-10-18 14:14:39 +0100

    removed files

--- project/settings.py
+++ project/settings.py
@@ -121,13 +121,6 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR/ "static"
-]
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
